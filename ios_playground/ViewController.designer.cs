a78a4df8bf1b2b2925c107aef0d304398d86e0cc
Sabit Huraira <sabitzhabit@gmail.com>
2016-10-27 19:45:35 +0700

    first commit

---
+++ ios_playground/ViewController.designer.cs
@@ -0,0 +1,38 @@
+// WARNING
+//
+// This file has been generated automatically by Xamarin Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+using UIKit;
+
+namespace ios_playground
+{
+    [Register ("ViewController")]
+    partial class ViewController
+    {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel Label_count { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField Txt_Name { get; set; }
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (Label_count != null) {
+                Label_count.Dispose ();
+                Label_count = null;
+            }
+
+            if (Txt_Name != null) {
+                Txt_Name.Dispose ();
+                Txt_Name = null;
+            }
+        }
+    }
+}
